ad057c4a2b278e201ecf2ce7f615210c71fce564
aayusharora <aayush113002@gmail.com>
2017-04-22 02:07:06 +0530

    added python -fl

---
+++ controllers/profile.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# try something like
+
+@auth.requires_login()
+
+def save():
+    profilerows = db(db.profile.created_by == session.auth.user.id).select(orderby =~ db.profile.id)
+    for x in profilerows:
+        db.profile.farm_name.default = x.farm_name
+        db.profile.farm_address.default = x.farm_address
+        db.profile.farm_website.default = x.farm_website
+        break;
+        
+    form = SQLFORM(db.profile).process()
+    return locals()
---
+++ models/menu.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Customize your APP title, subtitle and menus here
+# ----------------------------------------------------------------------------------------------------------------------
+
+response.logo = A(B('web', SPAN(2), 'py'), XML('&trade;&nbsp;'),
+                  _class="navbar-brand", _href="http://www.web2py.com/",
+                  _id="web2py-logo")
+response.title = request.application.replace('_', ' ').title()
+response.subtitle = ''
+
+# ----------------------------------------------------------------------------------------------------------------------
+# read more at http://dev.w3.org/html5/markup/meta.name.html
+# ----------------------------------------------------------------------------------------------------------------------
+response.meta.author = myconf.get('app.author')
+response.meta.description = myconf.get('app.description')
+response.meta.keywords = myconf.get('app.keywords')
+response.meta.generator = myconf.get('app.generator')
+
+# ----------------------------------------------------------------------------------------------------------------------
+# your http://google.com/analytics id
+# ----------------------------------------------------------------------------------------------------------------------
+response.google_analytics_id = None
+
+# ----------------------------------------------------------------------------------------------------------------------
+# this is the main application menu add/remove items as required
+# ----------------------------------------------------------------------------------------------------------------------
+
+response.menu = [
+    (T('Home'), False, URL('products', 'view'), [])
+]
+
+DEVELOPMENT_MENU = True
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# provide shortcuts for development. remove in production
+# ----------------------------------------------------------------------------------------------------------------------
+
+def _():
+    # ------------------------------------------------------------------------------------------------------------------
+    # shortcuts
+    # ------------------------------------------------------------------------------------------------------------------
+    app = request.application
+    ctr = request.controller
+    # ------------------------------------------------------------------------------------------------------------------
+    # useful links to internal and external resources
+    # ------------------------------------------------------------------------------------------------------------------
+    response.menu += [
+        (T('Post'), False, URL('products','post')),
+        (T('Profile'), False, URL('profile','save')),
+    ]
+
+
+if DEVELOPMENT_MENU:
+    _()
+
+if "auth" in locals():
+    auth.wikimenu()
